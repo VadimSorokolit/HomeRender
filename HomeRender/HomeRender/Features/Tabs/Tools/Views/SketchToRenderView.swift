c6cb80592a7f142e672a029370e58b661502edae
Vadym Sorokolit <macintosh@ukr.net>
2026-06-18 20:28:08 +0300

    Added bottom action buttons

--- HomeRender/HomeRender/Features/Tabs/Tools/Views/SketchToRenderView.swift
+++ HomeRender/HomeRender/Features/Tabs/Tools/Views/SketchToRenderView.swift
@@ -11,7 +11,100 @@ struct SketchToRenderView: View {
     let card: ToolCard?
     
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            GlobalConstants.bgColor
+                .ignoresSafeArea()
+            
+        }
+        .safeAreaInset(edge: .bottom) {
+            HStack {
+                VStack(spacing: 16) {
+                    HStack {
+                        Button(action: {
+                            print("Resize button did tap")
+                        }, label: {
+                            RoundedRectangle(cornerRadius: 40)
+                                .fill(Color.white)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 48)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 40)
+                                        .stroke(Color(hex: 0xCECED0), lineWidth: 1)
+                                }
+                                .overlay {
+                                    HStack(spacing: 6) {
+                                        Image(.resize)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundStyle(.black)
+                                            .frame(size: 20)
+                                        
+                                        Text("Resize")
+                                            .foregroundStyle(.black)
+                                            .font(.system(size: 14, weight: .medium))
+                                    }
+                                }
+                        })
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            print("Save button did tap")
+                        }, label: {
+                            RoundedRectangle(cornerRadius: 40)
+                                .fill(Color.white)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 48)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 40)
+                                        .stroke(Color(hex: 0xCECED0), lineWidth: 1)
+                                }
+                                .overlay {
+                                    HStack(spacing: 6) {
+                                        Image(.save)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundStyle(.black)
+                                            .frame(size: 20)
+                                        
+                                        Text("Save")
+                                            .foregroundStyle(.black)
+                                            .font(.system(size: 14, weight: .medium))
+                                    }
+                                }
+                        })
+                    }
+                    
+                    Button(action: {
+                        print("Regenerate button did tap")
+                    }, label: {
+                        RoundedRectangle(cornerRadius: 40)
+                            .fill(.black)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 56)
+                            .overlay {
+                                HStack(spacing: 8) {
+                                    Image(.arrow)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .foregroundStyle(.white)
+                                        .frame(size: 24)
+                                    
+                                    Text("Regenerate")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 24, weight: .medium))
+                                }
+                            }
+                    })
+                }
+                .padding()
+            }
+            .frame(height: 152)
+        }
     }
     
+}
+
+#Preview {
+    SketchToRenderView(card: nil)
 }
